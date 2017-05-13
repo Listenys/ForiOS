7eaadd4b1d260d02845473021421b038220747cb
Listen <443662526@qq.com>
2017-05-13 13:57:11 +0800

    for iOS

--- RBR/RBR/main.c
+++ RBR/RBR/main.c
@@ -9,16 +9,30 @@
 #include <stdio.h>
 #include <math.h>
 
+void meterToFeetAndInches(double meters,unsigned int *ftPtr,double * inPtr){
+    //假定meters的值非负
+    
+    double rawFeet = meters * 3.281;//将meters变量的值转化为feet的值，类型为浮点数
+    
+    unsigned int feet = (unsigned int)floor(rawFeet);//计算类型为无符号整型feet变量的值
+    
+    printf("Storing %u to the address %p\n",feet,ftPtr);
+    *ftPtr = feet;
+    
+    double fractioalFoot = rawFeet - feet;
+    double inches = fractioalFoot * 12.0;
+    
+    printf("Storing %.2f to the address %p\n",inches,inPtr);
+    *inPtr = inches;
+}
+
 int main(int argc, const char * argv[]) {
-    double pi = 3.14;
-    double integerPart;
-    double fractionPart;
+    double meters = 3.0;
+    unsigned int feet;
+    double inches;
     
-    //将integerPart的地址作为实参传入
-    fractionPart = modf(pi,&integerPart);//pi的整数部分拷贝至intergerPart的地址中，小数部分作为函数的返回值赋值给fractionPart
-    
-    //获取interger地址上的值
-    printf("intergerPart = %.0f,fractionPart = %.2f\n",integerPart,fractionPart);
+    meterToFeetAndInches(meters, &feet,&inches);
+    printf("%.1f meters is equal to %d feet and %.1f inches.",meters,feet,inches);
     
     return 0;
 }
