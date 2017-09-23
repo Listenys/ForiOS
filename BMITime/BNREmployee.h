d6ba6cfb58bb3b31998d9535c61258d63697ea53
Listen <443662526@qq.com>
2017-09-23 17:50:58 +0800

    keep learning

--- BMITime/BNREmployee.h
+++ BMITime/BNREmployee.h
@@ -2,7 +2,11 @@
 //  BNREmployee.h
 //  BMITime
 //
+<<<<<<< Updated upstream
 //  Created by ListenS on 17/9/20.
+=======
+//  Created by Listen on 17/9/14.
+>>>>>>> Stashed changes
 //  Copyright © 2017年 Listen. All rights reserved.
 //
 
@@ -11,8 +15,13 @@
 @interface BNREmployee : BNRPerson
 
 @property (nonatomic) unsigned int employeeID;
+<<<<<<< Updated upstream
 //@property (nonatomic) unsigned int officeAlarmCode;
 @property (nonatomic) NSDate *hireDate;
+=======
+@property (nonatomic) unsigned int officeAlarmCode;
+@property (nonatomic) NSDate *hirDate;
+>>>>>>> Stashed changes
 - (double)yearsOfEmployment;
 
 @end
--- BMITime/BNREmployee.m
+++ BMITime/BNREmployee.m
@@ -2,12 +2,17 @@
 //  BNREmployee.m
 //  BMITime
 //
+<<<<<<< Updated upstream
 //  Created by ListenS on 17/9/20.
+=======
+//  Created by Listen on 17/9/14.
+>>>>>>> Stashed changes
 //  Copyright © 2017年 Listen. All rights reserved.
 //
 
 #import "BNREmployee.h"
 
+<<<<<<< Updated upstream
 //类扩展
 @interface BNREmployee ()
 @property (nonatomic) unsigned int officelAlarmCode;
@@ -35,4 +40,8 @@
     return normalBMI * 0.9;
 }
 
+=======
+@implementation BNREmployee
+
+>>>>>>> Stashed changes
 @end
